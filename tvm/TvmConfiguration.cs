481f52bdcf1dbc27617eeb419f0cd443006feb14
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-09-05 04:40:17 +0300

    Implementation of Builder pattern

---
+++ tvm/TvmConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace tvm
+{
+    /// <summary>
+    /// Class contains tvm settings
+    /// </summary>
+    internal sealed class TvmConfiguration
+    {
+        public readonly string[] SourceFilePaths;
+
+        public readonly bool Interpretation;
+
+        public readonly bool Compilation;
+
+        public TvmConfiguration(string[] sourceFilePaths, bool interpretation, bool compilation)
+        {
+            SourceFilePaths = sourceFilePaths;
+            Interpretation = interpretation;
+            Compilation = compilation;
+        }
+    }
+}
