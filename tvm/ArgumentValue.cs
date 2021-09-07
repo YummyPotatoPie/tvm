8fb8afc7770e1744449bf824d6e0ba4864f270e5
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-09-07 13:11:06 +0300

    Added doc

---
+++ tvm/ArgumentValue.cs
@@ -0,0 +1,22 @@
+﻿namespace tvm
+{
+    /// <summary>
+    /// Class represents argument like: -c -v
+    /// </summary>
+    public class ArgumentValue
+    {
+        public string Argument { get; private set; }
+
+        public ArgumentValue(string argument) => Argument = argument;
+    }
+
+    /// <summary>
+    /// Class represents arguments like: --compile=true
+    /// </summary>
+    public class NameArgumentValue : ArgumentValue
+    {
+        public string ArgumentValue { get; private set; }
+
+        public NameArgumentValue(string argument, string argumentValue) : base(argument) => ArgumentValue = argumentValue;
+    }
+}
