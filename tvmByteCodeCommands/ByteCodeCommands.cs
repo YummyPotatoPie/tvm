692287b2f814c3f116ee4ab358178a823368cc26
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-09-19 00:55:47 +0300

    Changed field signature

--- tvmByteCodeCommands/ByteCodeCommands.cs
+++ tvmByteCodeCommands/ByteCodeCommands.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 
 namespace tvmByteCodeCommands
 {
-    public class ByteCodeCommands
+    public static class ByteCodeCommands
     {
-        public Dictionary<string, int> Commands { get; private set; } = new Dictionary<string, int>
+        public static Dictionary<string, byte> Commands { get; private set; } = new Dictionary<string, byte>
         {
-            { "push" , 0x1 },
-            { "pop" , 0x2 },
-            { "add" , 0x3 },
-            { "sub" , 0x4 },
-            { "mul" , 0x5 },
-            { "div" , 0x6 }
+            { "push",   0x1 },
+            { "pop",    0x2 },
+            { "add",    0x3 },
+            { "sub",    0x4 },
+            { "mul",    0x5 },
+            { "div",    0x6 }
         };
     }
 }
