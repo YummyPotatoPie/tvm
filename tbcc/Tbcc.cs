bb5b41ef5e405c4d87e5eead7934ccc920f85856
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-09-19 00:54:28 +0300

    Complete Tiny Byte Code Compiler

--- tbcc/Tbcc.cs
+++ tbcc/Tbcc.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System;
+
+using tvmByteCodeParser;
+using tvmByteCodeCommands;
+
 using CommandLine;
 
 namespace tbcc
@@ -18,24 +22,30 @@ namespace tbcc
 
         public static void Main(string[] args)
         {
-            string[] sourceFiles;
+            string[] sourceFiles = { };
 
             Parser.Default.ParseArguments<ParserOptions>(args).WithParsed(option =>
             {
                 List<string> parsedFiles = new();
                 foreach (string filename in option.SourceFiles)
                 {
-                    if (File.Exists(filename) && Path.GetExtension(filename) == ".tbc") parsedFiles.Add(filename);
-                    else
+                    if (!File.Exists(filename))
                     {
-                        Console.WriteLine("Invalid extention of source file");
-                        return;
+                        Console.WriteLine("File does not exist: " + filename);
+                        Environment.Exit(1);
                     }
+                    else if (!(Path.GetExtension(filename) == ".tbc"))
+                    {
+                        Console.WriteLine("Invalid extention of source file: " + filename);
+                        Environment.Exit(2);
+                    }
+                    else parsedFiles.Add(filename);
                 }
                 sourceFiles = parsedFiles.ToArray();
             });
 
-
+            CompileByteCodeFiles(sourceFiles);
+            Console.WriteLine("Compilation complete");
         }
 
         private static void CompileByteCodeFiles(string[] sourceFiles)
@@ -43,11 +53,41 @@ namespace tbcc
             foreach (string sourceFile in sourceFiles)
             {
                 StreamReader reader = new(sourceFile);
-                BinaryWriter binaryWriter = new(File.Open(sourceFile + "c", FileMode.OpenOrCreate));
 
-                binaryWriter.Write(new ByteCodeParser(reader.ReadToEnd()).Parse());
-                binaryWriter.Close();
+                string[] commands = new ByteCodeParser(reader.ReadToEnd()).Parse();
+                BinaryWriter compiledCodeWriter = new(File.Open(sourceFile + "c", FileMode.OpenOrCreate));
+
+                compiledCodeWriter.Write(GetBytesFromByteCode(commands));
+                compiledCodeWriter.Close();
             }
+        }
+
+        private static byte[] GetBytesFromByteCode(string[] commands)
+        {
+            Dictionary<string, byte> commandsOpCodes = ByteCodeCommands.Commands;
+            List<byte> bytes = new();
+
+            foreach (string command in commands)
+            {
+                if (commandsOpCodes.ContainsKey(command)) bytes.Add(commandsOpCodes[command]);
+                else
+                {
+                    try
+                    {
+                        int value = int.Parse(command);
+                        byte[] valueBytes = BitConverter.GetBytes(value);
+
+                        foreach (byte valueByte in valueBytes) bytes.Add(valueByte);
+                    }
+                    catch (ArgumentException)
+                    {
+                        Console.WriteLine("Invalid number value");
+                        Environment.Exit(3);
+                    }
+                }
+            }
+
+            return bytes.ToArray();
         }
     }
 }
