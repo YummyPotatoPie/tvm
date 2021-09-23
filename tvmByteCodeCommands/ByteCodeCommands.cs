43440151d2f475a0ef14d0295dd7d1c59f119b81
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-09-23 17:18:31 +0300

    Separated commands

--- tvmByteCodeCommands/ByteCodeCommands.cs
+++ tvmByteCodeCommands/ByteCodeCommands.cs
@@ -5,9 +5,13 @@ namespace tvmByteCodeCommands
 {
     public static class ByteCodeCommands
     {
-        public static Dictionary<string, byte> Commands { get; private set; } = new Dictionary<string, byte>
+        public static Dictionary<string, byte> CommandsWithArgument { get; private set; } = new Dictionary<string, byte>
         {
-            { "push",   0x1 },
+            { "push",   0x1 }
+        };
+
+        public static Dictionary<string, byte> CommandsWithoutArgument { get; private set; } = new Dictionary<string, byte>
+        {
             { "pop",    0x2 },
             { "add",    0x3 },
             { "sub",    0x4 },
