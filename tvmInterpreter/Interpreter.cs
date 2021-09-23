c0136943cdf0be8c34598cc825fc705f2e7045dc
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-09-23 17:25:16 +0300

    Added interpreter for byte code

--- tvmInterpreter/Interpreter.cs
+++ tvmInterpreter/Interpreter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+using tvmByteCodeCommands;
 using tvmInterpreter.Memory;
 
 namespace tvmInterpreter
@@ -12,11 +13,71 @@ namespace tvmInterpreter
 
         private Interpreter() => MemoryStack = new();
 
-        public Interpreter(byte[] byteCode) : base() => Commands = new(byteCode);
+        public Interpreter(byte[] byteCode) : this() => Commands = new(byteCode);
+
+        public Interpreter(byte[] byteCode, int size) : this(byteCode) => MemoryStack = new(size);
 
         public void Interpret()
         {
+            while (!Commands.EndOfProgram())
+            {
+                byte currentCommand = Commands.GetCurrentCommand();
+                Commands.Next();
 
+                if (ByteCodeCommands.CommandsWithArgument.ContainsValue(currentCommand))
+                {
+                    CommandWithArgumentHandle(currentCommand);
+                }
+                else if (ByteCodeCommands.CommandsWithoutArgument.ContainsValue(currentCommand))
+                {
+                    CommandWithoutArgumentHandle(currentCommand);
+                }
+                else throw new ArgumentException("Invalid command or value");
+            }
+        }
+
+        private void CommandWithoutArgumentHandle(byte command)
+        {
+            switch (command)
+            {
+                case 0x2:
+                    MemoryStack.Pop();
+                    break;
+                case 0x3:
+                    BinaryCommandHandle(BinaryCommands.Add);
+                    break;
+                case 0x4:
+                    BinaryCommandHandle(BinaryCommands.Sub);
+                    break;
+                case 0x5:
+                    BinaryCommandHandle(BinaryCommands.Mul);
+                    break;
+                case 0x6:
+                    BinaryCommandHandle(BinaryCommands.Div);
+                    break;
+            }
+        }
+
+        private void CommandWithArgumentHandle(byte command)
+        {
+            switch (command)
+            {
+                case 0x1:
+                    MemoryStack.Push(Commands.GetValue());
+                    break;
+            }
+        }
+
+        private void BinaryCommandHandle(Func<int, int, int> command)
+        {
+            int a = GetValueFromStack(), b = GetValueFromStack();
+            MemoryStack.Push(command(b, a));
+        }
+
+        private int GetValueFromStack()
+        {
+            int value = MemoryStack.Peek(); MemoryStack.Pop();
+            return value;
         }
     }
 }
