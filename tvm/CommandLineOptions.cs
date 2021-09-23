ff3237c91abe95e9480133b706b1ee0115fb3cf0
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-09-23 17:22:36 +0300

    Tvm options

--- tvm/CommandLineOptions.cs
+++ tvm/CommandLineOptions.cs
@@ -7,9 +7,15 @@ namespace tvm
     internal sealed class CommandLineOptions
     {
         [Value(index: 0, Required = true, HelpText = "Byte code source files")]
-        public IEnumerable<string> ByteCodeFiles { get; private set; }
+        public string ByteCodeFile { get; private set; }
 
         [Option(shortName: 'i', longName: "interpret", Required = false, HelpText = "Sets interpretation mode", Default = true )]
         public bool Interpretation { get; private set; }
+
+        [Option(shortName: 'c', longName: "compile", Required = false, HelpText = "Sets compilation mode", Default = false)]
+        public bool Compilation { get; private set; }
+
+        [Option(longName: "compiled-run", Required = false, HelpText = "Run .tbcc source files", Default = false)]
+        public bool CompiledRun { get; private set; }
     }
 }
