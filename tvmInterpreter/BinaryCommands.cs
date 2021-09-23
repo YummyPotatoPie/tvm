a1454927a567100260f60ed43cabd4beb4f8b5ac
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-09-23 17:26:09 +0300

    Added methods for commands: add, sub, mul, div

---
+++ tvmInterpreter/BinaryCommands.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace tvmInterpreter
+{
+    public static class BinaryCommands
+    {
+        public static int Add(int a, int b) => a + b;
+
+        public static int Sub(int a, int b) => a - b;
+
+        public static int Mul(int a, int b) => a * b;
+
+        public static int Div(int a, int b) => b == 0 ? throw new ArgumentException("Division by zero") : a / b;
+    }
+}
