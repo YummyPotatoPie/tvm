96c9308272299ae0037345f76d8a0201f095dc49
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-09-29 00:28:18 +0300

    Almost done

--- tvmByteCodeCommands/ByteCodeCommands.cs
+++ tvmByteCodeCommands/ByteCodeCommands.cs
@@ -13,12 +13,13 @@ namespace tvmByteCodeCommands
         /// </summary>
         public static Dictionary<string, byte> CommandsWithArgument { get; private set; } = new Dictionary<string, byte>
         {
-            { "push",   0x1 },
-            { "peek",   0x8 },
-            { "preg",   0x9 },
-            { "ptrfc",  0xA },
-            { "vpop",   0xB },
-            { "int",    0xC }
+            { "push",   0x1  },
+            { "peek",   0x8  },
+            { "preg",   0x9  },
+            { "ptrfr",  0xA  },
+            { "vpop",   0xB  },
+            { "int",    0x14 },
+            { "ptrfw",  0x1A }
         };
 
         /// <summary>
@@ -26,15 +27,39 @@ namespace tvmByteCodeCommands
         /// </summary>
         public static Dictionary<string, byte> CommandsWithoutArgument { get; private set; } = new Dictionary<string, byte>
         {
-            { "pop",    0x2 },
-            { "add",    0x3 },
-            { "sub",    0x4 },
-            { "mul",    0x5 },
-            { "div",    0x6 },
-            { "dup",    0x7 },
-            { "cmp",    0xD },
-            { "shiftr", 0xE },
-            { "shiftl", 0xF }
+            { "pop",    0x2  },
+            { "add",    0x3  },
+            { "sub",    0x4  },
+            { "mul",    0x5  },
+            { "div",    0x6  },
+            { "dup",    0x7  },
+            { "cmp",    0xC  },
+            { "shiftr", 0xD  },
+            { "shiftl", 0xE  },
+            { "xor",    0xF  },
+            { "or",     0x10 },
+            { "and",    0x11 }
+        };
+
+        /// <summary>
+        /// Jump commands
+        /// </summary>
+        public static Dictionary<string, byte> JumpCommands { get; private set; } = new Dictionary<string, byte>
+        {
+            { "jl",     0x15 },
+            { "jb",     0x16 },
+            { "je",     0x17 },
+            { "jne",    0x18 },
+            { "jmp",    0x19 }
+        };
+
+        /// <summary>
+        /// Special commands of Tiny Byte Code
+        /// </summary>
+        public static Dictionary<string, byte> SpecialCommands { get; private set; } = new Dictionary<string, byte>
+        {
+            { "label",  0x12 },
+            { "call",   0x13 },
         };
     }
 }
